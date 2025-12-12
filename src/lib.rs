bfccc1b67bca7e0eb16e1696286f40b3d7cc9dd0
瑶 <916586595@qq.com>
2025-12-12 22:13:06 +0800

    feat: 拓展导出函数及命名空间

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,100 @@
 pub mod localisation;
 pub mod script;
 
-use crate::script::script::{parse_str, serialize_ast, Item};
+use crate::localisation::localisation::{
+    parse_str as parse_loc_str, serialize_ast as serialize_loc_ast,
+};
+use crate::script::script::{parse_str as parse_scr_str, serialize_ast as serialize_scr_ast, Item};
 use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 
 #[wasm_bindgen]
-pub fn parse(content: &str) -> Result<JsValue, JsValue> {
-    let ast = parse_str(content).map_err(|e| JsValue::from_str(&e))?;
-    to_value(&ast).map_err(|e| JsValue::from_str(&e.to_string()))
+pub struct Script;
+
+#[wasm_bindgen]
+impl Script {
+    /// 从字符串解析 AST
+    ///
+    /// # Arguments
+    ///
+    /// * `content`: 文件内容
+    ///
+    /// returns: Result<JsValue, JsValue>
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///
+    /// ```
+    #[wasm_bindgen]
+    pub fn parse(content: &str) -> Result<JsValue, JsValue> {
+        let ast = parse_scr_str(content).map_err(|e| JsValue::from_str(&e))?;
+        to_value(&ast).map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+
+    /// 序列化 AST 为字符串
+    ///
+    /// # Arguments
+    ///
+    /// * `json`: 条目列表
+    ///
+    /// returns: Result<String, JsValue>
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///
+    /// ```
+    #[wasm_bindgen]
+    pub fn serialize(json: JsValue) -> Result<String, JsValue> {
+        let ast: Vec<Item> = from_value(json).map_err(|e| JsValue::from_str(&e.to_string()))?;
+        let content = serialize_scr_ast(&ast);
+        Ok(content)
+    }
 }
 
 #[wasm_bindgen]
-pub fn serialize(json: JsValue) -> Result<String, JsValue> {
-    let ast: Vec<Item> = from_value(json).map_err(|e| JsValue::from_str(&e.to_string()))?;
-    let content = serialize_ast(&ast);
-    Ok(content)
+pub struct Localisation;
+
+#[wasm_bindgen]
+impl Localisation {
+    /// 根据字符串解析 AST
+    ///
+    /// # Arguments
+    ///
+    /// * `content`: 文件内容
+    ///
+    /// returns: Result<JsValue, JsValue>
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///
+    /// ```
+    #[wasm_bindgen]
+    pub fn parse(content: &str) -> Result<JsValue, JsValue> {
+        let ast = parse_loc_str(content).map_err(|e| JsValue::from_str(&e))?;
+        to_value(&ast).map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+
+    /// 将 AST 序列化为本地化内容
+    ///
+    /// # Arguments
+    ///
+    /// * `json`: AST
+    ///
+    /// returns: Result<String, JsValue>
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///
+    /// ```
+    #[wasm_bindgen]
+    pub fn serialize(json: JsValue) -> Result<String, JsValue> {
+        let ast = from_value(json).map_err(|e| JsValue::from_str(&e.to_string()))?;
+        let content = serialize_loc_ast(&ast);
+        Ok(content)
+    }
 }
